aa67e57c63238e5ff98a8e2adf7fcc43e1eba770
AnnaLisik <macfanpl@interia.pl>
2020-04-29 10:41:28 +0200

    zmodyfikowano komentarz

--- php/inc/modern.js
+++ php/inc/modern.js
@@ -1,4 +1,4 @@
-// Sample usage instruction for modernizr.js
+// To tylko templatka. Do rozbudowania. Wersja alpha. Błędy zgłaszać jako issues na GH
 import modernizr from "modernizr";
 modernizr.build({}, function(result){
     console.log("Build started");
