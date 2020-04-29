743dfc10bcb5dc572baf14db4dc9bb79af1e6c47
AnnaLisik <macfanpl@interia.pl>
2020-04-29 15:48:01 +0200

    clean up

--- php/inc/modern.js
+++
@@ -1,6 +0,0 @@
-// To tylko templatka. Do rozbudowania. Wersja alpha. Błędy zgłaszać jako issues na GH
-import modernizr from "modernizr";
-modernizr.build({}, function(result){
-    console.log("Build started");
-}
-)
