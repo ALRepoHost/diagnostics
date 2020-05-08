ae3f2aaaf770b7963ab9a705a568b6288236ecb3
wlisik.pl <macfanpl@interia.pl>
2020-05-08 21:46:09 +0200

    TestSuite invoker in Bash

---
+++ php/tests/bash/TestSuiteInvoker.sh
@@ -0,0 +1,22 @@
+!#/bin/bash
+echo "Enviroment test suite....."
+
+# Test function
+phar_tester () {
+    echo "Check test engine existance and permissions....."
+    if (-f ["*.phar"]){
+        if ("ls -alh *.phar | grep xargs" == 777){
+            echo "Executing tests...."
+            command = "php -r *.php"
+            eval $command
+        } else {
+            echo "Wrong permissions are set. Unable to continue"
+            break
+        }
+    } else {
+        echo "Test file not present. Unable to continue"
+        break
+    }
+}
+
+# phar_tester()
