927b33bf19712e5e057aeacd490e42bad2ae9e24
wlisikpl <macfanpl@interia.pl>
2020-05-30 23:22:52 +0200

    xdebug

---
+++ php/tests/xdebug.php
@@ -0,0 +1,16 @@
+<?php
+define('PDEBUGGER', 'x-debug');
+$tpu = 1;
+$delimeter = ".";
+$dickacy = $_ENV['fuckyoumozilla'];
+
+
+class debugTest{
+    
+    function __construct(){
+        $this->$dickacy = $dickacy;
+        $this->$tpu = $tpu;
+        $this->$delimeter = $delimeter;
+    }
+}
+?>
