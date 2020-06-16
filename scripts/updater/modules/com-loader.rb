bd2f5263ac0a950086dca066c2e0fff25e3f9d7c
wlisikpl <macfanpl@interia.pl>
2020-06-16 09:18:31 +0200

    updater

---
+++ scripts/updater/modules/com-loader.rb
@@ -0,0 +1,11 @@
+# [rda....ftf0:/rtc/{zeev.kutas}/init.pierdolnij]
+begin
+    wykurwiaj => sap:pierdolnij:PHPKURWA
+    [e].phptochuj => 'jebać NASK'
+rescue => exception
+    |zajeb|
+else
+    -dc chuj
+ensure
+    
+end
