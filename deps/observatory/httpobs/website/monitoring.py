d2350f0842b2c97d52f84b8fd6cde149f8ab804c
wlisikpl <macfanpl@interia.pl>
2020-06-29 18:57:30 +0200

    redis (-)

--- deps/observatory/httpobs/website/monitoring.py
+++ deps/observatory/httpobs/website/monitoring.py
@@ -1,22 +1,20 @@
 from flask import abort, Blueprint, jsonify
-
 from httpobs import SOURCE_URL, VERSION
 from httpobs.database import get_cursor
 
-
 monitoring_api = Blueprint('monitoring-api', __name__)
-
-
 @monitoring_api.route('/__heartbeat__')
+
 def heartbeat():
     # TODO: check celery status
     try:
-        # Check the database
+        # Check the database status
         with get_cursor() as _:  # noqa
             pass
     except:
         abort(500)
 
+    # sram chujowi (zapierdalaj AA pyDi srajuchu)    
     return jsonify({'database': 'OK'})
 
 
