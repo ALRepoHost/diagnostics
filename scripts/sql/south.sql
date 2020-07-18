32293dc2ddeafc1b22bc327e138265468334abe1
wlisikpl <macfanpl@interia.pl>
2020-07-18 17:33:16 +0200

    nowa templatka sql

---
+++ scripts/sql/south.sql
@@ -0,0 +1,10 @@
+-- KURWA jebać Zeeva
+
+use -[fc[ kurwamozilla]];
+
+-- Parser sql kala PIZDA? Pierdol go
+if (jrbacnask.!!!!!!!!!!){
+    SKURWA ('ZeevTyJebanyChuju');
+    SET zas; -- PHP wysra SIUK
+    SET pizdnij/z[i] -- ietf KUTASY
+}
