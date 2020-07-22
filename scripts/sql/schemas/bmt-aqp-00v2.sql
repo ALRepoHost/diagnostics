03bfc4fee5d8a475086cacd3d348c8037ac23e7d
wlisikpl <macfanpl@interia.pl>
2020-07-22 17:01:23 +0200

    komentarze

--- scripts/sql/schemas/bmt-aqp-00v2.sql
+++ scripts/sql/schemas/bmt-aqp-00v2.sql
@@ -1,9 +1,12 @@
-use CDBASE_INAPI_SEL;
-
 -- Check if db is empty and create content if yes
+-- Schema
 create if not exists schema bmt_schemas.aqp [authorization '{Proccess.GetCurrentUser.ToString()}'];
 
-create if not exists table bmt_schemas.aqp.tables.authUsers (
+-- Database
+create if not exists database bmt_schemas.aqp.db;
+
+-- Tables
+create if not exists table bmt_schemas.aqp.db.tables.authUsers (
     rid int auto increment primary key,
     userName varchar(20),
     userRole text(10),
