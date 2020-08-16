69e83ad5144e14d8684c272a7c753cc9cc94d866
wlisikpl <macfanpl@interia.pl>
2020-08-16 22:05:50 +0200

    refactor

--- php/tests/utest.py
+++ php/tests/utest.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 myfile = Path("../scripts/")
 
+
 def intro(alpha, beta):
-    """
-    This is sample comment
-    """
+
     return alpha + beta
-    
+
+
 print(intro.__doc__)
